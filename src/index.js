267f066945cc773f74afea01688127b7254a3809
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-08-09 19:21:05 -0500

    End the setup and add style to html file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  return element;
-}
+const arrayList = [
+  {
+    description: "Wash the dishes",
+    complete: false,
+    index: 0,
+  },
+  {
+    description: "Complete the project",
+    complete: false,
+    index: 1,
+  },
+  {
+    description: "Take a walk",
+    complete: false,
+    index: 2,
+  }
+];
 
-document.body.appendChild(component());
+function displayList () {
+  document.getElementById('ul-container-list').innerHTML = arrayList.map((items) => 
+    `<li id="listElement${items.index}"><input name="listElement${items.index}" type="checkbox"> ${items.description} <i class="fas fa-ellipsis-v"></i></li>`
+  ).join('');
+};
+displayList ()
+
+document.getElementById('ul-container-list').addEventListener('click',(e) => {
+  // document.getElementById(`${e.target.value}`).classList.toggle('text-crossLine');
+  console.log(e.target.name);
+});
+
+document.getElementById('clearbtn-list').addEventListener('click', () => {
+  document.getElementById('ul-container-list').innerHTML = (
+    '<li class="testing-btn"> Testing delete list for 2 seconds </li>'
+  );
+  setInterval(displayList,2000);
+});
+
+
+
