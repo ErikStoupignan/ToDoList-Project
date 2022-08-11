b2abcb7c9c0b94237ae264fbbf4fdd6354aa96ba
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-08-10 23:48:33 -0500

    Add last details to the project

--- src/addAndRemove.js
+++ src/addAndRemove.js
@@ -1,44 +1,48 @@
 
-let arrayList = [
-  {
-    description: 'Wash the dishes',
-    complete: false,
-    index: 0,
-  },
-  {
-    description: 'Fly for the sky',
-    complete: false,
-    index: 0,
-  }
-];
+let arrayList = [];
 
 export default class Crud  {
-  constructor (description, status = false , index ) {
+  constructor (description, index , status = false) {
     this.description = description;
     this.complete = status;
     this.index = index;
   }
 
   static add () {
+
     if(localStorage.getItem('data')){
       const oldElement = JSON.parse(localStorage.getItem('data'));
       arrayList = oldElement;
     }
 
-    console.log(arrayList.length);
-
     const input = document.getElementById('input-list').value;
-    if( input === '') return arrayList;
+    if( input === '' ) return arrayList;
 
     document.getElementById('input-list').value= '';
-
+     
     arrayList.push(new Crud(input));
+    this.update();
+
+
+    console.log(arrayList.length);
     localStorage.setItem('data', JSON.stringify(arrayList));
     return arrayList;
   }
 
-  static delete () {
+  static delete (index) {
 
+    console.log('Borrar app:',index);
+    arrayList.splice(index,1);
+    this.update();
+    localStorage.setItem('data', JSON.stringify(arrayList));
+    return arrayList;
+
+  }
+
+  static update () {
+    for ( let i = 0 ; i < arrayList.length ; i++ ){
+      arrayList [i].index = i;
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,36 +1,31 @@
 // import _ from 'lodash';
+// import { add } from 'lodash';
 import './style.css';
 import Crud  from './addAndRemove.js';
-import { add } from 'lodash';
+import Modal  from './modalMenu.js';
+
+// Review if we have information in the local Storage
 displayList(Crud.add());
 
 // Print the list
-function displayList(input) {
+export function displayList(input) {
   document.getElementById('ul-container-list').innerHTML = input.map((items,index) => 
-  `<li id="${index}"><input name="listElement${index}" type="checkbox"> ${items.description} <i id="icono${index}" class="fas fa-ellipsis-v"></i></li>
+  `
+  <li id="${index}"><input id="listElement${index}" type="checkbox"> 
+  <input id='input${index}' value="${items.description}" type="text" disabled="disabled" class='input-list'> 
+  <i id="iconDots${index}" class="fas fa-ellipsis-v"></i></li>
   `).join('');
-}
+};
 
 // Call the function Add
-document.getElementById('input-list').addEventListener('change', (e) => {
+document.getElementById('input-list').addEventListener('change', () => {
   displayList(Crud.add());
-});
-
-
-// Close de delet menu
-document.getElementById('delet-btn-eachElement').addEventListener('click', () => {
-  document.getElementById('delet-Container-eachelement').classList.add('display-none');
-});
-
-document.getElementById('delet-x-icon').addEventListener('click', () => {
-  document.getElementById('delet-Container-eachelement').classList.add('display-none');
 });
 
 // Refresh the list complete
 const refreshIcons = document.getElementById('refresh-list');
 const updatingMessage = document.getElementById('updating-list-message');
 refreshIcons.addEventListener('click', () => {
-
   displayList(Crud.add());
   updatingMessage.classList.remove('display-none');
   refreshIcons.classList.add('fa-spin');
@@ -39,5 +34,25 @@ refreshIcons.addEventListener('click', () => {
     updatingMessage.classList.add('display-none');
   }, 1000);
 });
+
+// Open de Option menu
+document.getElementById('ul-container-list').addEventListener('click', (e) => {
+    const { id } = e.target;
+    let firstTime = true;
+    let regex = /(?<=iconDots)\d+$/;
+
+    if(firstTime) {
+      if (regex.test(id)) {
+        firstTime = false;
+        const index = id.match(regex)[0];
+        Modal.startModal(index);
+      }
+    } else {
+      firstTime = true;
+    }
+  }
+);
+
+
 
 
